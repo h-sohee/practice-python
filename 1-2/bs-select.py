d4b4ed6c8886b6d479b6ce4794956617f02b34a0
sh6863 <kristen6863@gmail.com>
2021-02-16 18:06:03 +0900

    .

--- 1-2/bs-select.py
+++ 1-2/bs-select.py
@@ -2,16 +2,18 @@ from bs4 import BeautifulSoup
 
 # 분석 대상 HTML --- (※1)
 html = """
-<html><body>
-<div id="meigen">
-    <h1>위키북스 도서</h1>
-    <ul class="items">
-        <li>유니티 게임 이펙트 입문</li>
-        <li>스위프트로 시작하는 아이폰 앱 개발 교과서</li>
-        <li>모던 웹사이트 디자인의 정석</li>
-    </ul>
-</div>
-</body></html>
+<html>
+    <body>
+        <div id="meigen">
+            <h1>위키북스 도서</h1>
+            <ul class="items">
+                <li>유니티 게임 이펙트 입문</li>
+                <li>스위프트로 시작하는 아이폰 앱 개발 교과서</li>
+                <li>모던 웹사이트 디자인의 정석</li>
+            </ul>
+        </div>
+    </body>
+</html>
 """
 
 # HTML 분석하기 --- (※2)
@@ -19,12 +21,17 @@ soup = BeautifulSoup(html, 'html.parser')
 
 # 필요한 부분을 CSS 퀴리로 추출하기
 # 타이틀 부분 추출하기 --- (※3)
-h1 = soup.select_one("div#meigen > h1").string
+h1 = soup.select_one("div#meigen > h1").string      # 글자
+# h1 = soup.select_one("div#meigen > h1")             # 요소
+# h1 = soup.select_one("h1").string                   # 글자
 print('h1 =', h1)
+
+# ul = soup.select_one("ul")
+# print(f"ul : {ul}")
+# print(ul.attrs)
 
 # 목록부분 추출하기 --- (※4)
 li_list = soup.select("div#meigen > ul.items > li")
 for li in li_list:
     print('li =', li.string)
-
 
