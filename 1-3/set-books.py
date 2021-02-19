f2205b18e680d6a270fafb5f311428f2fd845289
sh6863 <kristen6863@gmail.com>
2021-02-19 15:03:53 +0900

    .

--- 1-3/set-books.py
+++ 1-3/set-books.py
@@ -5,17 +5,17 @@ soup = BeautifulSoup(fp, "html.parser")
 
 # CSS 선택자로 검색하는 방법
 sel = lambda q: print(soup.select_one(q).string)
-sel("#nu")
-sel("li#nu")
-sel("ul > li#nu")
-sel("#bible #nu")
-sel("#bible > #nu")
-sel("ul#bible > li#nu")
-sel("li[id='nu']")
-sel("li:nth-of-type(4)")
+sel("#nu")                              # (※1)
+sel("li#nu")                            # (※2)
+sel("ul > li#nu")                       # (※3)
+sel("#bible #nu")                       # (※4)
+sel("#bible > #nu")                     # (※5)
+sel("ul#bible > li#nu")                 # (※6)
+sel("li[id='nu']")                      # (※7)
+sel("li:nth-of-type(4)")                # (※8)
 
 # 그 밖의 방법
-print(soup.select("li")[3].string)
-print(soup.find_all("li")[3].string)
+print(soup.select("li")[3].string)      # (※9)
+print(soup.find_all("li")[3].string)    # (※10)
 
 
